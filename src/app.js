939b65bd713be9b1dabe924a1dc8c228cab805f0
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-01-30 20:27:12 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return who[whoIndx] + " " + what[whatIndx] + " " + when[whenIndx];
 };
